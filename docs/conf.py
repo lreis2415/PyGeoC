bc07af61516e695cff87f086b82927d9bd8125bf
crazyzlj <crazyzlj@gmail.com>
2017-09-15 00:06:17 +0800

    change sphinx verion

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ source_parsers = {
     '.md': CommonMarkParser
 }
 
-needs_sphinx = '1.5'
+needs_sphinx = '1.6.3'
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.todo',
