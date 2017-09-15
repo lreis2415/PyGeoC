558b55484dcb996031c7967cafd33de91e274c91
crazyzlj <crazyzlj@gmail.com>
2017-09-15 10:33:42 +0800

    debug on macos

--- reinstall.sh
+++ reinstall.sh
@@ -4,7 +4,13 @@
 #
 cd $PWD
 rm -r dist
-pip install tox
+python -m pip install tox
 python setup.py bdist_wheel --python-tag py2
 cd dist
-for i in `find . -name *.whl`; do pip install $i  --upgrade; done
+if python -c "import pygeoc" &> /dev/null; then
+    echo 'PyGeoC has been installed, and will be uninstalled first.'
+    python -m pip uninstall PyGeoC
+else
+    echo 'PyGeoC will be firstly installed.'
+fi
+for i in `find . -name *.whl`; do python -m pip install $i --upgrade; done
