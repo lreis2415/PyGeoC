b89b07fd4ebf526953833dbcb95917e6eb03700d
crazyzlj <crazyzlj@gmail.com>
2017-09-22 22:36:48 +0800

    update

--- examples/ex04_watershed_delineation.py
+++ examples/ex04_watershed_delineation.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 # Exercise 4: Delineate watershed based on TauDEM functions with PyGeoC
 
-from pygeoc.TauDEM import TauDEMWorkflow
+from pygeoc.TauDEM import TauDEMWorkflow, TauDEMFilesUtils, TauDEM
 
 
 def main():
     """The simplest usage of watershed delineation based on TauDEM."""
     dem = '../tests/data/Jamaica_dem.tif'
     num_proc = 2
+
     TauDEMWorkflow.watershed_delineation(num_proc, dem)
 
 
