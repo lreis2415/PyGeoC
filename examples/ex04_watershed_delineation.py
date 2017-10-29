779ed89ecc04f4255bcf05fb2d7a5759489d28ed
crazyzlj <crazyzlj@gmail.com>
2017-10-29 15:48:20 +0800

    update

--- examples/ex04_watershed_delineation.py
+++ examples/ex04_watershed_delineation.py
@@ -9,7 +9,7 @@ def main():
     """The simplest usage of watershed delineation based on TauDEM."""
     dem = '../tests/data/Jamaica_dem.tif'
     num_proc = 2
-    wp = '../tests/data/tmpresults/wtsd_delineation'
+    wp = '../tests/data/tmp_results/wtsd_delineation'
 
     TauDEMWorkflow.watershed_delineation(num_proc, dem, workingdir=wp)
 
